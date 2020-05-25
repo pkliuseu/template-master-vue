cc9b8362eb79b3db935d91fb11df793fea34d11b
weilan <17796646068@163.com>
2020-05-25 08:27:53 +0800

    feat: server增加post请求中间件

--- _server/index.js
+++ _server/index.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = new Koa()
 const cors = require('koa2-cors');
 const routes = require('./api')
-// const httpProxy = require('http-proxy-middleware');
+const bodyParser = require('koa-bodyparser');
 
 // 设置开启跨域
 app.use(cors({
@@ -13,12 +13,9 @@ app.use(cors({
   credentials: true, // 标示该响应是合法的
 }));
 
-// 代理第三方网站跨域
-/* app.use('/', httpProxy({
-  // 代理跨域目标接口
-  target: 'https://cn.bing.com/',
-  changeOrigin: true,
-})); */
-
+// 引入post中间件
+app.use(bodyParser());
+// 注册路由
 app.use(routes.routes(), routes.allowedMethods());
+// 监听端口
 app.listen(3000)
